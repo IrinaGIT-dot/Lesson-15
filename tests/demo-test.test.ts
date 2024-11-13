1f5010d3c6a879e6146740116b7bbce077f18bb9
irinabolshakova <irina.bolshakov.ee@gmail.com>
2024-11-13 19:52:48 +0200

    Initial commit

---
+++ tests/demo-test.test.ts
@@ -0,0 +1,24 @@
+import {expect, test} from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+    const path = require('path');
+    const filePath = `file://${path.resolve('dummy-order.html')}`;
+    await page.goto(filePath);
+
+    //const filePath = path.resolve('dummy-order.html');
+    //await page.goto(`${baseURL}/${filePath}`);
+})
+
+test('Button Place your order is disabled', async ({ page }) => {
+    await expect(page.getByTestId("submit-order")).toBeDisabled()
+    }
+)
+
+test('Button Place your order is enabled', async ({ page }) => {
+    const userNameField = page.getByTestId("username")
+    const userEmailField = page.getByTestId("email")
+    await userEmailField.fill("er@er.ty")
+    await userNameField.fill("test")
+    await expect(page.getByTestId("submit-order")).toBeEnabled()
+    }
+)
